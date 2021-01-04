d9684028f4c97a80e0fd1f5d0bb0cb503bfddc1b
Sean Oyler <xrysen@gmail.com>
2021-01-04 21:16:41 +0000

    Adds validators

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {only_integer: true, other_than: 0 }
+  validate :must_carry_mens_or_womens
+
+  def must_carry_mens_or_womens
+    if mens_apparel == false && womens_apparel == false
+      errors.add("Must carry one of womens or mens apparel")
+    end
+  end
 end
