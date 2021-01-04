668f27c3ed5705b24ec5883b13d5f90b4db81e32
Sean Oyler <xrysen@gmail.com>
2021-01-04 23:00:01 +0000

    Prevents store that has employees from being destroyed

---
+++ exercises/exercise_9.rb
@@ -0,0 +1,22 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+require_relative './exercise_6'
+
+@store1 = Store.find(1)
+if @store1.destroy
+  puts "Store destroyed! It has #{@store1.employees.size} =/"
+else
+  puts "Could not destroy store :)"
+end
+
+# Make sure empty stores can be destroyed
+@empty_store = Store.create!(name: 'Test Empty Store', annual_revenue: 1, mens_apparel: true, womens_apparel: false)
+if @empty_store.destroy
+  puts "Empty Store destroyed! This is good"
+else
+  puts "Whoa! Empty store should be destroyable... Not cool!"
+end
--- lib/store.rb
+++ lib/store.rb
@@ -4,10 +4,18 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: {only_integer: true, other_than: 0 }
   validates :mens_apparel, :womens_apparel, inclusion: [true, false]
   validate :must_carry_mens_or_womens
+  before_destroy :check_if_employees
 
   def must_carry_mens_or_womens
     if mens_apparel == false && womens_apparel == false
       errors.add(:must_carry_mens_or_womens, ":one of these needs to be true")
     end
   end
+
+  private
+    def check_if_employees
+      if self.employees.count() > 0
+        return false
+      end
+    end
 end
