dd7669d22af8275a50eec3692a775637eeed14ed
Sean Oyler <xrysen@gmail.com>
2021-01-04 21:33:07 +0000

    Adds more validations and tests to confirm they are working

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Create a store"
+store_name = gets.chomp
+new_store = Store.create(name: store_name, annual_revenue: 30000, mens_apparel: false, womens_apparel: true)
+if !new_store.valid?
+  puts new_store.errors.full_messages
+end
--- lib/store.rb
+++ lib/store.rb
@@ -2,11 +2,12 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: {only_integer: true, other_than: 0 }
+  validates :mens_apparel, :womens_apparel, inclusion: [true, false]
   validate :must_carry_mens_or_womens
 
   def must_carry_mens_or_womens
     if mens_apparel == false && womens_apparel == false
-      errors.add("Must carry one of womens or mens apparel")
+      errors.add(:must_carry_mens_or_womens, ":one of these needs to be true")
     end
   end
 end
